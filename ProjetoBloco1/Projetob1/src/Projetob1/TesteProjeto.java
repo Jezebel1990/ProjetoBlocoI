c268ee8027b929f3056a277f2ab2b98793a0dde1
Jezebel1990 <75287031+Jezebel1990@users.noreply.github.com>
2021-04-14 23:06:40 -0300

    Últimos ajustes

--- ProjetoBloco1/Projetob1/src/Projetob1/TesteProjeto.java
+++ ProjetoBloco1/Projetob1/src/Projetob1/TesteProjeto.java
@@ -11,10 +11,11 @@ public class TesteProjeto {
 			Musicas musicas = new Musicas();
 			Jogos jogos = new Jogos();
 			AtividadesFisicas atividadesFisicas = new AtividadesFisicas();
+		        Livros livros = new Livros();
 			Listas lista = null;
 			
 
-			System.out.println("Bem vinde ao nosso menu! Vamos tirar o seu tÈdio nessa pandemia :D");
+			System.out.println("Bem vinde ao nosso menu! Vamos tirar o seu t√©dio nessa pandemia :D");
 			
 			System.out.println("\nInsira um NickName: ");
 			String nome = scanner.nextLine();
@@ -25,55 +26,83 @@ public class TesteProjeto {
 			int continuar = 1;
 			while(continuar==1) {
 				System.out.println("\n__________________________________________________________");
-				System.out.println("\nOi "+usuario.getNome()+", informe o cÛdigo da aÁ„o desejada:"
+				        +"\nOi " + usuario.getNome()
+					+ ", informe o c√≥digo da sess√£o desejada:"
 					+ "\n1- Assistir filmes"
-					+ "\n2- Assistir sÈries"
-					+ "\n3- Ouvir m˙sica"
+					+ "\n2- Assistir s√©ries"
+					+ "\n3- Ouvir m√∫sica"
 					+ "\n4- Jogar jogos"
-					+ "\n5- Praticar atividades fÌsicas"
+					+ "\n5- Praticar atividades f√≠sicas" + "\n6- Ler um livro"
 					+"\n________________________________________________________________________");
 				int opcao = scanner.nextInt();
 				switch(opcao) {
-					case 1: lista = filmes;break;
-					case 2: lista = series;break;
-					case 3: lista = musicas;break;
-					case 4: lista = jogos;break;
-					case 5: lista = atividadesFisicas;break;
-					default:System.out.println("OpÁ„o inv·lida");break;
+					case 1:
+						lista = filmes;
+						break;
+					case 2: 
+						lista = series;
+						break;
+					case 3: 
+						lista = musicas;
+						break;
+					case 4: 
+						lista = jogos;
+						break;
+					case 5: lista = atividadesFisicas;
+						break;
+					default: {
+						System.out.println("Op√ß√£o inv√°lida");
+						lista = null;
+						break;
 				}
-				
+				}
 				if (lista != null) {
 					lista.listaGeneros();
 					int genero = scanner.nextInt();
 					switch(genero) {
-						case 1: System.out.println("\nSugestıes:\n" + lista.lista1());break;
-						case 2: System.out.println("\nSugestıes:\n" + lista.lista2());break;
-						case 3: System.out.println("\nSugestıes:\n" + lista.lista3());break;
-						case 4: System.out.println("\nSugestıes:\n" + lista.lista4());break;
-						case 5: System.out.println("\nSugestıes:\n" + lista.lista5());break;
-						case 6: System.out.println("\nSugestıes:\n" + lista.lista6());break;
-						default:System.out.println("\nOpÁ„o inv·lida");break;
-					}
-					
-				}
-				try {
-					
+						case 1:
+							System.out.println("\nSugest√µes:\n" + lista.lista1());
+							break;
+						case 2:
+							System.out.println("\nSugest√µes:\n" + lista.lista2());
+							break;
+						case 3:
+							System.out.println("\nSugest√µes:\n" + lista.lista3());
+							break;
+						case 4:
+							System.out.println("\nSugest√µes:\n" + lista.lista4());
+							break;
+						case 5:
+							System.out.println("\nSugest√µes:\n" + lista.lista5());
+							break;
+						case 6: 
+							System.out.println("\nSugest√µes:\n" + lista.lista6());
+							break;
+						default:
+							System.out.println("\nOp√ß√£o inv√°lida");
+							break;	
+				           }
+				
+				       }
 					System.out.println("\n"+usuario.getNome()+", deseja fazer outra atividade?"
-							+ "\n0- N„o."
-							+ "\n1- Sim! Me mostre as opÁıes!");
+							+ "\n0- N√£o."
+							+ "\n1- Sim! Me mostre as op√ß√µes!");
 					continuar = scanner.nextInt();
-					if(continuar != 0 &&continuar != 1) {
-						throw new IllegalArgumentException();
-					}
-					}
-					catch(IllegalArgumentException e){
-						System.out.println("VocÍ digitou um valor inv·lido");
+					while(continuar != 0 &&continuar != 1) {
+						try {
+						    if (continuar != 0 && continuar != 1) {
+						       throw new IllegalArgumentException();
+					            }
+						} catch(IllegalArgumentException e) {
+						System.out.println("Voc√™ digitou um valor inv√°lido. Por favor, digite novamente.");
 						System.out.println("\n"+usuario.getNome()+", deseja fazer outra atividade?"
-								+ "\n0- N„o."
-								+ "\n1- Sim! Me mostre as opÁıes!");
+								+ "\n0- N√£o."
+								+ "\n1- Sim! Me mostre as op√ß√µes!");
 						continuar = scanner.nextInt();
+						}
+					     }
 					}
-					}System.out.println("\n"+usuario.getNome()+", obrigade! AtÈ mais");
+					System.out.println("\n"+usuario.getNome()+", obrigade! At√© mais");
 		}
 
 	}
